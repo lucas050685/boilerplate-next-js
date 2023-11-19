67387eeb8355299885f46051617d9f1f45245b61
Lucas Machado Rodrigues <lucas050685@gmail.com>
2023-11-19 14:37:33 -0300

    feat: adding vercel deplyment script

--- scripts/vercel-deployment.sh
+++ scripts/vercel-deployment.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-echo "...deplyment in vercel";
-yarn vercel --token $VERCEL_TOKEN;
+echo "";
+echo "Deplyment in vercel...";
+
+if [ "${VERCE_TOKEN}" == '' ]
+then
+  echo "";
+  echo "VERCEL_TOKEN is required to deploy the application";
+  echo "Be sure you have this variable declared in you evnironment";
+  echo "";
+  exit 1;
+fi
+
+yarn vercel --prod --token $VERCEL_TOKEN;
